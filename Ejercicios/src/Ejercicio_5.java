bfe903f935d77961208bf02b0a817cccb05da3e6
karen chaparro <ckarendaniela133@gmail.com>
2026-07-27 17:21:19 -0500

    solucion del Ejercicio_5

---
+++ Ejercicios/src/Ejercicio_5.java
@@ -0,0 +1,10 @@
+public class Ejercicio_5 {
+    public static void main (String[] args){
+        int i = 1;
+
+        do {
+            System.out.println("7 x " + i + "=" + (7 * i));
+            i++;
+        }while (i <= 12);
+    }
+}

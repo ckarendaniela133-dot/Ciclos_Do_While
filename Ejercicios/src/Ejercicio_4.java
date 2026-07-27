47338c7f17b8ff483aac1ad1ff31e0e43b1362c6
karen chaparro <ckarendaniela133@gmail.com>
2026-07-27 17:07:19 -0500

    solucion del Ejercicio_4

---
+++ Ejercicios/src/Ejercicio_4.java
@@ -0,0 +1,10 @@
+public class Ejercicio_4 {
+    public static void main(String[] args){
+        int i = 20;
+
+        do {
+            System.out.println(i);
+            i = i -2;
+        }while (i >= 0);
+    }
+}
